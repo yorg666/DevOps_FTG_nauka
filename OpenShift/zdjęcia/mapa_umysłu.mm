415bc29046ee76e4343ccd427fa057024a449825
Krystian Nowaczyk <krystian.nowaczyk@gft.com>
2016-10-06 17:28:57 +0200

    Kubernetes i Docker

---
+++ OpenShift/zdjęcia/mapa_umysłu.mm
@@ -0,0 +1,85 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1475763379919" ID="ID_783811191" MODIFIED="1475763531980">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;Obraz Dockera
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;+
+    </p>
+    <p>
+      &#160;Dockerfile
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1475763410988" HGAP="62" ID="ID_984910427" MODIFIED="1475763529458" POSITION="right" TEXT="Kontener">
+<node CREATED="1475763536684" ID="ID_928380050" MODIFIED="1475763897065">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pody
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1475763787284" HGAP="83" ID="ID_1935527452" MODIFIED="1475764401117" TEXT="znajduj&#x105; si&#x119; fizycznie na Nodach" VSHIFT="744">
+<node CREATED="1475763580937" HGAP="11" ID="ID_1673959011" MODIFIED="1475764388064" VSHIFT="55">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nody
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1475763847257" ID="ID_1545077344" MODIFIED="1475763863888" TEXT="s&#x105; zarz&#x105;dzane przez mastera">
+<node CREATED="1475763929761" HGAP="81" ID="ID_1311487054" MODIFIED="1475763942113" TEXT="Master" VSHIFT="19">
+<node CREATED="1475763945079" HGAP="23" ID="ID_528653816" MODIFIED="1475763963444" TEXT="zarz&#x105;dza Autentykacj&#x105;" VSHIFT="42"/>
+<node CREATED="1475763965631" ID="ID_619201471" MODIFIED="1475763992102" TEXT="zarz&#x105;dza data persistance"/>
+<node CREATED="1475763996474" ID="ID_1253207432" MODIFIED="1475764008900" TEXT="dostarcza Scheduler">
+<node CREATED="1475764010667" HGAP="28" ID="ID_1683004425" MODIFIED="1475764039107" TEXT="zarz&#x105;dza umiejscowieniem Pod&#xf3;w na nodach" VSHIFT="60"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1475763814221" HGAP="81" ID="ID_639025479" MODIFIED="1475764373478" TEXT="s&#x105; pogrupowane w Serwisy" VSHIFT="-254">
+<node CREATED="1475764133304" HGAP="35" ID="ID_1097089738" MODIFIED="1475764299025" TEXT="Service" VSHIFT="-74">
+<node CREATED="1475764080970" HGAP="27" ID="ID_168990626" MODIFIED="1475764179843" TEXT="grupuj&#x105; pody" VSHIFT="3">
+<node CREATED="1475764097018" ID="ID_1129791329" MODIFIED="1475764100028" TEXT="" VSHIFT="43"/>
+</node>
+<node CREATED="1475764199463" ID="ID_565602457" MODIFIED="1475764210860" TEXT="s&#x105; dost&#x119;pne pod IP"/>
+<node CREATED="1475764213874" ID="ID_1606137476" MODIFIED="1475764220929" TEXT="s&#x105; dost&#x119;pn&#x119; pod nazw&#x105; DNS"/>
+</node>
+</node>
+<node CREATED="1475764248264" HGAP="89" ID="ID_213091925" MODIFIED="1475764405504" VSHIFT="-671">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Za ich cykl &#380;ycia i klonowanie odpowiada&#160;&#160;
+    </p>
+    <p>
+      Management/Replication controller
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
